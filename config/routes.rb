b48e0f9192692ea0004914fd31e075ec190cfde1
John Briceno <jpbriceno16@gmail.com>
2017-06-28 17:58:39 +0000

    added about us page, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/about' => 'page#about'
   root 'page#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
